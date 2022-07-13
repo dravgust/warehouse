138208111aac07efa8fcb35f4f9e3bb4323b6cf5
anton <anton@vayosoft.com>
2022-07-13 12:32:29 +0300

    ...

---
+++ Warehouse.Core/Entities/Models/Payloads/CustomPayload.cs
@@ -0,0 +1,12 @@
+﻿using Vayosoft.Core.SharedKernel.Entities;
+
+namespace Warehouse.Core.Entities.Models.Payloads
+{
+    public class CustomPayload
+    {
+        public DateTime ReportedAt { get; set; }
+        public string DeviceType { get; set; }
+        public string MacAddress { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Warehouse.Core/UseCases/IPS/Queries/GetBeaconPayload.cs
@@ -0,0 +1,10 @@
+﻿using Vayosoft.Core.Queries;
+using Warehouse.Core.Entities.Models.Payloads;
+
+namespace Warehouse.Core.UseCases.IPS.Queries
+{
+    public class GetBeaconPayload : IQuery<BeaconPayload>
+    {
+        public string MacAddress { set; get; }
+    }
+}
