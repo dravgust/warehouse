eaa2c7d8b3e2614321c9a1380e4239414369079a
anton <anton@vayosoft.com>
2022-07-18 10:25:22 +0300

    init

---
+++ Warehouse.API/Services/ImportExportService.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using Warehouse.Core.Entities.Models;
+using Warehouse.Core.UseCases.Products.Commands;
+
+namespace Warehouse.API.Services
+{
+    public class ImportExportService
+    {
+        public List<SetProduct> ImportProducts(byte[] data)
+        {
+            var result = new List<SetProduct>();
+            using var ms = new MemoryStream(data);
+            var wb = new XSSFWorkbook(ms);
+            ISheet excelSheet = wb.GetSheetAt(0);
+
+            for (var i = 0; i <= excelSheet.LastRowNum; i++)
+            {
+                try
+                {
+                    IRow row = excelSheet.GetRow(i);
+                    var name = row.GetCell(0).ToString();
+                    if (!string.IsNullOrEmpty(name))
+                    {
+                        var description = row.GetCell(1)?.ToString();
+                        result.Add(new SetProduct
+                        {
+                            Name = name,
+                            Description = description ?? string.Empty,
+                        });
+                    }
+                }
+                catch (Exception e)
+                {
+                    Trace.TraceError(e.Message);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Warehouse.Core/UseCases/IPS/Models/BeaconTelemetryDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Warehouse.Core.UseCases.IPS.Models
+{
+    public class BeaconTelemetryDto
+    {
+        public int RSSI { get; set; }
+        public int TxPower { get; set; }
+        public int Battery { get; set; }
+        public double? Temperature { get; set; }
+        public double? Humidity { get; set; }
+        public double? X0 { get; set; }
+        public double? Y0 { get; set; }
+        public double? Z0 { get; set; }
+    }
+}
