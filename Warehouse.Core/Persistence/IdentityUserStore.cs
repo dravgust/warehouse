0d7480f6b19d6568736d67a22f7cd150fb8e4f5e
dravgust <dravgust@hotmail.com>
2022-08-05 09:51:31 +0300

    init

--- Warehouse.Core/Persistence/IdentityUserStore.cs
+++ Warehouse.Core/Persistence/IdentityUserStore.cs
@@ -15,7 +15,48 @@ namespace Warehouse.Core.Persistence
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        public dynamic GetUserRoles(object userId)
+        public async Task<List<SecurityRoleEntity>> GetRolesAsync(List<ulong> providers)
+        {
+            var rl = await EmbeddedRolesAsync();
+            var roles = await _context.Set<SecurityRoleEntity>()
+                .Where(r => r.ProviderId != null && providers.Contains(r.ProviderId.Value)).ToListAsync();
+            rl.AddRange(roles);
+            return rl;
+        }
+
+        public Task<List<SecurityRoleEntity>> EmbeddedRolesAsync()
+        {
+            return _context.Set<SecurityRoleEntity>().Where(r => r.ProviderId == null).ToListAsync();
+        }
+
+        public Task<SecurityRoleEntity> GetRoleAsync(string roleId)
+        {
+            return _context.Set<SecurityRoleEntity>().FirstOrDefaultAsync(r => r.Id == roleId);
+        }
+
+        public Task<List<SecurityObjectEntity>> GetObjectsAsync()
+        {
+            return _context.Set<SecurityObjectEntity>().ToListAsync();
+        }
+
+        public dynamic GetRolePermissionsAsync(string roleId)
+        {
+            var query = from rp in _context.Set<SecurityRolePermissionsEntity>()
+                join so in _context.Set<SecurityObjectEntity>() on rp.ObjectId equals so.Id
+                where rp.RoleId == roleId
+                select new
+                {
+                    Id = rp.Id,
+                    RoleId = rp.RoleId,
+                    ObjectId = rp.ObjectId,
+                    ObjectName = so.Name,
+                    Permissions = rp.Permissions
+                };
+
+            return query.ToListAsync();
+        }
+
+        public dynamic GetUserRolesAsync(object userId)
         {
             var query = (from ur in _context.Set<UserRoleEntity>()
                 join r in _context.Set<SecurityRoleEntity>() on ur.RoleId equals r.Id
@@ -27,6 +68,7 @@ namespace Warehouse.Core.Persistence
                     Description = r.Description,
                     Items = (from rp in _context.Set<SecurityRolePermissionsEntity>()
                         join so in _context.Set<SecurityObjectEntity>() on rp.ObjectId equals so.Id
+                        where rp.RoleId == r.Id
                         select new
                         {
                             Id = rp.Id,
@@ -34,10 +76,10 @@ namespace Warehouse.Core.Persistence
                             ObjectId = rp.ObjectId,
                             ObjectName = so.Name,
                             Permissions = rp.Permissions
-                        }).Where(rp => rp.RoleId == r.Id).ToList()
+                        }).ToList()
                 });
 
-            return query.ToList();
+            return query.ToListAsync();
         }
 
         public Task<UserEntity> FindByIdAsync(object userId, CancellationToken cancellationToken)
