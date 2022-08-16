dbfc92ef6d9cd4504b54266982a5ace35a38ba31
anton <anton@vayosoft.com>
2022-08-16 09:06:22 +0300

    init

---
+++ Vayosoft.Caching/Configuration.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Vayosoft.Caching.Redis;
+using Vayosoft.Core.Caching;
+
+namespace Vayosoft.Caching
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddCaching(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMemoryCache();
+
+            var redisConnectionString = configuration.GetConnectionString("RedisConnectionString");
+
+            services.AddOptions<CachingOptions>().Bind(configuration.GetSection("Caching")).ValidateDataAnnotations();
+
+
+            if (!string.IsNullOrEmpty(redisConnectionString))
+            {
+                services.AddOptions<RedisCachingOptions>().Bind(configuration.GetSection("Caching:Redis")).ValidateDataAnnotations();
+
+                services.AddSingleton<IDistributedMemoryCache, RedisMemoryCache>();
+            }
+            else
+            {
+                //Use MemoryCache decorator to use global platform cache settings
+                services.AddSingleton<IDistributedMemoryCache, MemoryCacheWrapper>();
+            }
+
+            return services;
+        }
+    }
+}
---
+++ Warehouse.API/Services/Swagger/Configuration.cs
@@ -0,0 +1,35 @@
+﻿namespace Warehouse.API.Services.Swagger
+{
+    public static class Configuration
+    {
+        public static IServiceCollection AddSwaggerService(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+            services.ConfigureOptions<ConfigureSwaggerGenOptions>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerService(this IApplicationBuilder app)
+        {
+            if (app == null)
+            {
+                throw new ArgumentNullException(nameof(app));
+            }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            //app.UseSwagger();
+            app.UseSwagger(c => c.RouteTemplate = "/swagger/api/{documentName}/swagger.json");
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/api/v1/swagger.json", "IPS Dashboard V1");
+                c.InjectStylesheet("/css/swagger.css");
+                c.RoutePrefix = "api";
+            });
+
+            return app;
+        }
+    }
+}
