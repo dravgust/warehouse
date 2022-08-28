876e158418b559eab663ea55d6b693e0beb0c00c
anton <anton@vayosoft.com>
2022-08-28 09:28:01 +0300

    init

--- Warehouse.Client/src/layouts/security/index.js
+++ Warehouse.Client/src/layouts/security/index.js
@@ -3,31 +3,50 @@ import DashboardNavbar from "../../examples/Navbars/DashboardNavbar";
 import DashboardLayout from "../../examples/LayoutContainers/DashboardLayout";
 import SuiBox from "../../components/SuiBox";
 import Footer from "../../examples/Footer";
-import { Grid } from "@mui/material";
+import { Card, Grid, Stack } from "@mui/material";
 import useSecurity, { SecurityPermissions } from "../../services/security-provider";
+import SecurityRoles from "./components/roles";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import { useState } from "react";
+import SecurityIcon from "@mui/icons-material/Security";
+import GppGoodIcon from "@mui/icons-material/GppGood";
 
-const SecurityRoles = React.lazy(() => import("./components/roles"));
 const SecurityObjects = React.lazy(() => import("./components/objects"));
 
 const Security = () => {
   const { hasPermissions } = useSecurity("USER", SecurityPermissions.Grant);
-
+  const [selectedView, setSelectView] = useState(0);
+  const handleChange = (event, value) => {
+    setSelectView(value);
+  };
   return (
     <DashboardLayout>
       <DashboardNavbar />
       {hasPermissions && (
         <SuiBox py={3}>
           <Grid container spacing={3}>
-            <Grid item xs={12} lg={6}>
-              <Suspense fallback={<p>Loading...</p>}>
+            <Grid item xs={12}>
+              <Card>
+                <SuiBox p={3}>
+                  <Tabs value={selectedView} onChange={handleChange} aria-label="navigation">
+                    <Tab icon={<SecurityIcon />} iconPosition="start" label="Roles" />
+                    <Tab icon={<GppGoodIcon />} iconPosition="start" label="Security Objects" />
+                  </Tabs>
+                </SuiBox>
+              </Card>
+            </Grid>
+            {selectedView === 0 ? (
+              <Grid item xs={12} lg={12}>
                 <SecurityRoles />
-              </Suspense>
-            </Grid>
-            <Grid item xs={12} lg={6}>
-              <Suspense fallback={<p>Loading...</p>}>
-                <SecurityObjects />
-              </Suspense>
-            </Grid>
+              </Grid>
+            ) : (
+              <Grid item xs={12} lg={12}>
+                <Suspense fallback={<>Loading...</>}>
+                  <SecurityObjects />
+                </Suspense>
+              </Grid>
+            )}
           </Grid>
         </SuiBox>
       )}
