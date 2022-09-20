840d181ddc8cc224e8b0d5ad2940aa6d0c47bb03
anton <anton@vayosoft.com>
2022-09-20 08:05:15 +0300

    init

---
+++ Warehouse.Benchmarks/Casting.cs
@@ -0,0 +1,108 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Warehouse.Benchmarks
+{
+    [MemoryDiagnoser(false)]
+    public class Casting
+    {
+        //[Benchmark]
+        //public Person HardCast()
+        //{
+        //    var antHardCast = (Person) StaticObject.Person;
+        //    return antHardCast;
+        //}
+
+        //[Benchmark]
+        //public Person SafeCast()
+        //{
+        //    var antHardCast = StaticObject.Person as Person;
+        //    return antHardCast!;
+        //}
+
+        //[Benchmark]
+        //public Person MatchCast()
+        //{
+        //    if (StaticObject.Person is Person person)
+        //    {
+        //        return person;
+        //    }
+
+        //    return null!;
+        //}
+
+
+        [Benchmark]
+        public List<Person> OfType()
+        {
+            return StaticObject.People.OfType<Person>().ToList();
+        }
+
+        [Benchmark]
+        public List<Person> HardCast_TypeOf()
+        {
+            return StaticObject.People
+                .Where(x => x.GetType() == typeof(Person)) 
+                .Select(x => (Person)x)
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<Person> HardCast_As()
+        {
+            return StaticObject.People
+                .Where(x => x as Person is not null)
+                .Select(x => (Person)x)
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<Person> HardCast_Is()
+        {
+            return StaticObject.People
+                .Where(x => x is Person)
+                .Select(x => (Person)x)
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<Person> Cast_As()
+        {
+            return StaticObject.People
+                .Where(x => x as Person is not null)
+                .Cast<Person>()
+                .ToList();
+        }
+
+        [Benchmark]
+        public List<Person> Cast_Is()
+        {
+            return StaticObject.People
+                .Where(x => x is Person)
+                .Cast<Person>()
+                .ToList();
+        }
+    }
+
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = default!;
+    }
+
+    public class StaticObject
+    {
+        public static object Person = new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString()
+        };
+
+        public static List<object> People = Enumerable
+            .Range(1, 10_000)
+            .Select(x => (object)new Person
+        {
+            Id = Guid.NewGuid(),
+            Name = Guid.NewGuid().ToString()
+        }).ToList();
+    }
+}
---
+++ Warehouse.Benchmarks/Ordering.cs
@@ -0,0 +1,50 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Warehouse.Benchmarks;
+
+[MemoryDiagnoser(false)]
+public class Ordering
+{
+    private static readonly Random R = new(80085);
+
+    [Params(100)]
+    public int Size { get; set; }
+
+    [Benchmark]
+    public int[] OrderBy()
+    {
+        var list = Enumerable.Range(1, Size).Select(r => R.Next(r));
+        return list.OrderBy(x => x).ToArray();
+    }
+
+    [Benchmark]
+    public int[] Sort()
+    {
+        var list = Enumerable.Range(1, Size).Select(r => R.Next(r)).ToArray();
+        Array.Sort(list);
+        return list;
+    }
+
+    [Benchmark]
+    public int[] Sort_Span()
+    {
+        Span<int> list = Enumerable.Range(1, Size).Select(r => R.Next(r)).ToArray();
+        list.Sort();
+        return list.ToArray();
+    }
+
+    [Benchmark]
+    public List<int> OrderByList()
+    {
+        var list = Enumerable.Range(1, Size).Select(r => R.Next(r));
+        return list.OrderBy(x => x).ToList();
+    }
+
+    [Benchmark]
+    public List<int> SortList()
+    {
+        var list = Enumerable.Range(1, Size).Select(r => R.Next(r)).ToList();
+        list.Sort();
+        return list;
+    }
+}
