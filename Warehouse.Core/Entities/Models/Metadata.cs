d7494eb0f06e920d35066cb33acba31d5f895ce9
anton <anton@vayosoft.com>
2022-09-22 09:06:33 +0300

    init

---
+++ Warehouse.Core/Entities/Models/Metadata.cs
@@ -0,0 +1,7 @@
+﻿namespace Warehouse.Core.Entities.Models
+{
+    public sealed class Metadata : List<MetadataItem>
+    { }
+
+    public sealed record MetadataItem(string Key, string Value, string Type, bool IsRequired);
+}
