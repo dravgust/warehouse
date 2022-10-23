7b3b7e7a3a57412d05b399d426b5642f265d9fa9
anton <anton@vayosoft.com>
2022-10-23 09:51:39 +0300

    init

---
+++ Warehouse.Host/MetricCollectorService.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using Vayosoft.Core.Utilities;
+
+namespace Warehouse.Host
+{
+    public class MetricCollectorService : IHostedService
+    {
+        private readonly ILogger<MetricCollectorService> _logger;
+
+        private BackgroundTask _task;
+
+        public MetricCollectorService(ILogger<MetricCollectorService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Hosted Service running.");
+
+            _task = new MetricsCollector(TimeSpan.FromSeconds(1000));
+            _task.Start();
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Hosted Service is stopping.");
+            await _task.StopAsync();
+        }
+    }
+
+    public class MetricsCollector : BackgroundTask
+    {
+        public MetricsCollector(TimeSpan interval) : base(interval)
+        { }
+
+        protected override Task ExecuteAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+            try
+            {
+                var report = new
+                {
+
+                };
+            }
+            catch (Exception e)
+            {
+                Trace.TraceWarning($"Telemetry| {e.Message}{Environment.NewLine}{e.StackTrace}");
+            }
+        }
+    }
+}
