60b5ada59f6a933a849c2e43074dd4f86de88564
dravgust <dravgust@hotmail.com>
2022-11-12 17:31:27 +0200

    .

--- Warehouse.Infrastructure/Persistence/AppDbContext.cs
+++ Warehouse.Infrastructure/Persistence/AppDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using Vayosoft.EF.MySQL;
 using Warehouse.Core.Domain.Entities;
+using Warehouse.Core.Domain.Enums;
 
 namespace Warehouse.Infrastructure.Persistence
 {
@@ -14,5 +15,22 @@ namespace Warehouse.Infrastructure.Persistence
         }
 
         public DbSet<UserEntity> Users => Set<UserEntity>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserEntity>().HasData(
+                new UserEntity("su")
+                {
+                    Id = 0,
+                    PasswordHash = "VBbXzW7xlaD3YiqcVrVehA==",
+                    Phone= "0508000000",
+                    Type = UserType.Supervisor,
+                    Registered = DateTime.UtcNow,
+                    ProviderId = 1000,
+                }
+            );
+        }
     }
 }
